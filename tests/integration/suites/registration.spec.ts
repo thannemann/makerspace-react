730341a833a2730c3d1c47c9a9d19c5e092b9515
thannemann <thannemann@gmail.com>
2026-04-05 17:08:51 -0400

    Update registration.spec.ts
    added missing " in the pending statements

--- tests/integration/suites/registration.spec.ts
+++ tests/integration/suites/registration.spec.ts
@@ -115,7 +115,7 @@ describe("Member management", () => {
     });
 
     it("Customers can register from home page via URL with discounts", async function () {
-      pending("Workaound for slow page load failures)
+      pending("Workaound for slow page load failures")
       this.timeout(200 * 1000);
       const discount = "SUNRISE-MONTH-50";
       await browser.url(utils.buildUrl(
@@ -221,7 +221,7 @@ describe("Member management", () => {
     });
 
     it("Admins can register a customer manually", async () => {
-      pending("Workaound for slow page load failures)
+      pending("Workaound for slow page load failures")
       const newMember = Object.assign({}, basicMembers.pop());
       const rejectionUid = "admin-register-home-page";
       await createRejectCard(rejectionUid);
